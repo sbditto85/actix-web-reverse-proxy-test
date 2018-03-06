d826a87c6d59044b056905c75aa0e494f5a2ef6d
Casey Allred <sbditto85@gmail.com>
2018-03-05 21:31:05 -0700

    welp it sorta works

---
+++ src/apperror.rs
@@ -0,0 +1,82 @@
+// Stolen and modified from https://github.com/JustinRyanH/actix-oauth2/blob/experimental_redis/src/errors.rs
+use std::fmt;
+use std::error::Error;
+
+use http;
+
+use actix_web::{self, Body, HttpResponse, StatusCode};
+use actix_web::client::SendRequestError;
+
+pub type AppResult<T> = Result<T, AppError>;
+
+#[derive(Debug)]
+pub enum AppError {
+    ActixWeb(actix_web::Error),
+    ActixSendError(actix_web::client::SendRequestError),
+    HttpError(http::Error),
+    Lazy(String),
+}
+
+impl AppError {
+    pub fn lazy<S: Into<String>>(s: S) -> AppError {
+        AppError::Lazy(s.into())
+    }
+}
+
+impl actix_web::error::ResponseError for AppError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR, Body::Empty)
+    }
+}
+
+impl fmt::Display for AppError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &AppError::ActixWeb(ref err) => write!(f, "ActixWeb Error: {:?}", err),
+            &AppError::ActixSendError(ref err) => write!(f, "ActixSend Error: {:?}", err),
+            &AppError::HttpError(ref err) => write!(f, "Http Error: {}", err),
+            &AppError::Lazy(ref err) => write!(f, "Lazy Error: {}", err),
+        }
+    }
+}
+
+impl Error for AppError {
+    fn description(&self) -> &str {
+        match *self {
+            AppError::ActixWeb(_) => "Errors invloving the Async Web Rust Framework",
+            AppError::ActixSendError(_) => "Couldn't connect to host",
+            AppError::HttpError(_) => "Error with http stuff",
+            AppError::Lazy(_) => "Errors I am too lazy to specify",
+        }
+    }
+}
+
+impl From<http::Error> for AppError {
+    fn from(v: http::Error) -> AppError {
+        AppError::HttpError(v)
+    }
+}
+
+impl From<String> for AppError {
+    fn from(v: String) -> AppError {
+        AppError::Lazy(v)
+    }
+}
+
+impl From<actix_web::Error> for AppError {
+    fn from(v: actix_web::Error) -> AppError {
+        AppError::ActixWeb(v)
+    }
+}
+
+impl From<SendRequestError> for AppError {
+    fn from(v: SendRequestError) -> AppError {
+        AppError::ActixSendError(v)
+    }
+}
+
+impl From<actix_web::error::PayloadError> for AppError {
+    fn from(v: actix_web::error::PayloadError) -> AppError {
+        AppError::Lazy(format!("PayloadError {:?}", v))
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,62 @@
+extern crate actix;
+extern crate actix_web;
+extern crate bytes;
+extern crate env_logger;
+extern crate futures;
+extern crate http;
+
+//use actix::Arbiter;
+use actix_web::*;
+use actix_web::client::*;
+use bytes::Bytes;
+use futures::Future;
+use futures::Stream;
+use futures::future::{FutureResult, result};
+use futures::stream::once;
+//use std::io::Write;
+use std::io::{self, ErrorKind};
+
+mod apperror;
+
+use apperror::*;
+
+
+fn index(_req: HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
+fn async_forward(_req: HttpRequest) -> Box<Future<Item=HttpResponse, Error=AppError>> {
+    client::ClientRequest::get("http://www.liveviewtech.com/")
+        .finish().unwrap()
+        .send()                         // <- connect to host and send request
+        .map_err(apperror::AppError::from)    // <- convert SendRequestError to an Error
+        .and_then(|resp| {              // <- we received client response
+            httpcodes::HttpOk.build()
+                .content_type("text/html")
+            // read one chunk from client response and send this chunk to a server response
+            // .from_err() converts PayloadError to a Error
+                .body(Body::Streaming(Box::new(resp.from_err())))
+                .map_err(|e| e.into()) // HttpOk::build() mayb return HttpError, we need to convert it to a Error
+        })
+        .responder()
+}
+
+fn main() {
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    let _ = env_logger::init();
+    let sys = actix::System::new("ws-example");
+
+    let _addr = HttpServer::new(
+        || Application::new()
+            // enable logger
+            .middleware(middleware::Logger::default())
+            .resource("/async", |r| r.route().a(async_forward))
+            .resource("/index.html", |r| r.f(|_| "Hello world!"))
+            .resource("/", |r| r.f(index)))
+        .threads(4)
+        .bind("127.0.0.1:8080").unwrap()
+        .start();
+
+    println!("Started http server: 127.0.0.1:8080");
+    let _ = sys.run();
+}
